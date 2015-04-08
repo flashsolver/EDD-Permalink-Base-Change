0658e57ab4ebe202a686ef0acbd7db0f3f400942
Karim Abdelwahab <karim@simplyhire.me>
2015-04-08 10:01:43 +0100

    EDD Permalink Base Change
    Changes /downloads to /templates for Easy digital downloads plugin (EDD)

---
+++ hide-admin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: EDD Slug
+Plugin URI: http://karimabdelwahab.com
+Description: Changes /downloads to /templates.
+Version: 1.0
+Author: Karim Abdelwahab
+Author URI: http://karimabdelwahab.com
+License: None
+*/
+
+define('EDD_SLUG', 'templates');
+
+?>
